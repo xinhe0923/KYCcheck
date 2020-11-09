594618d6955e0d3d12ac44f2cce676efcf96ae2c
xinhe0923 <yangxinhe0923@yahoo.com>
2020-11-09 14:33:05 +1100

    config file

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express, { Application, Request, Response, NextFunction } from "express";
 import axios from "axios";
 import bodyParser from "body-parser";
+import { Authorization } from "../config/config";
 
 var jsonParser = bodyParser.json();
 const app: Application = express();
@@ -8,25 +9,8 @@ const url: string =
   "https://australia-southeast1-reporting-290bc.cloudfunctions.net/driverlicence";
 const headers = {
   "Content-Type": "application/json",
-  Authorization:
-    "Bearer 03aa7ba718da920e0ea362c876505c6df32197940669c5b150711b03650a78cf",
+  Authorization: Authorization,
 };
-interface requestData {
-  birthDate: string;
-  givenName: string;
-  middleName: string;
-  familyName: string;
-  licenceNumber: string;
-  stateOfIssue: string;
-  expiryDate: string;
-}
-interface respondData {
-  verificationResultCode: string;
-}
-
-app.get("/hello", (req: Request, res: Response) => {
-  res.send("hello");
-});
 
 app.use((req: Request, res: Response, next: NextFunction) => {
   res.header("Access-Control-Allow-Origin", "*");
